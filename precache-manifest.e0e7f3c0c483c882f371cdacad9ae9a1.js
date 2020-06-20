4b9e1ce1a3c64181f316a62ca4a91c485b3ef288
ankit.vishnoi <ankit.vishnoi@servicenow.com>
2020-06-21 01:19:35 +0530

    Updates

---
+++ precache-manifest.e0e7f3c0c483c882f371cdacad9ae9a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995f83864b8f8f10ea3054ca9e145b28",
+    "url": "/akhileshapp/index.html"
+  },
+  {
+    "revision": "c4fef225897a2ddca6cb",
+    "url": "/akhileshapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc8c55f8227ad8bd8472",
+    "url": "/akhileshapp/static/js/2.ef14d195.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/akhileshapp/static/js/2.ef14d195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4fef225897a2ddca6cb",
+    "url": "/akhileshapp/static/js/main.68d46af7.chunk.js"
+  },
+  {
+    "revision": "bf5f7d6453833b7cbd3b",
+    "url": "/akhileshapp/static/js/runtime-main.c744d014.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/akhileshapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
